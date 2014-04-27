01e24a36e70754bbeac562d5851f4698043172f6
Miek Gieben <miek@miek.nl>
2014-04-27 16:40:08 +0100

    Add empty server_test.go file

--- server_test.go
+++ server_test.go
@@ -1,14 +1,16 @@
-// Copyright (c) 2013 The SkyDNS Authors. All rights reserved.
+// Copyright (c) 2014 The SkyDNS Authors. All rights reserved.
 // Use of this source code is governed by The MIT License (MIT) that can be
 // found in the LICENSE file.
 
-package server
+package main
+
+// etcd needs to be running on http://127.0.0.1:4001
 
 import (
+	/*
 	"bytes"
 	"encoding/json"
 	"github.com/miekg/dns"
-	"github.com/skynetservices/skydns/msg"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -18,207 +20,41 @@ import (
 	"strings"
 	"testing"
 	"time"
+	*/
 )
+
+func newTestServer(leader, secret, machines string) *server {
+	return nil
+}
+/*
+
+func newTestServerDNSSEC(leader, secret, nameserver string) *Server {
+	s := newTestServer(leader, secret, nameserver)
+	key, _ := dns.NewRR("skydns.local. IN DNSKEY 256 3 5 AwEAAaXfO+DOBMJsQ5H4TfiabwSpqE4cGL0Qlvh5hrQumrjr9eNSdIOjIHJJKCe56qBU5mH+iBlXP29SVf6UiiMjIrAPDVhClLeWFe0PC+XlWseAyRgiLHdQ8r95+AfkhO5aZgnCwYf9FGGSaT0+CRYN+PyDbXBTLK5FN+j5b6bb7z+d")
+	s.dnsKey = key.(*dns.DNSKEY)
+	s.keyTag = s.dnsKey.KeyTag()
+	s.privKey, _ = s.dnsKey.ReadPrivateKey(strings.NewReader(`
+Private-key-format: v1.3
+Algorithm: 5 (RSASHA1)
+Modulus: pd874M4EwmxDkfhN+JpvBKmoThwYvRCW+HmGtC6auOv141J0g6MgckkoJ7nqoFTmYf6IGVc/b1JV/pSKIyMisA8NWEKUt5YV7Q8L5eVax4DJGCIsd1Dyv3n4B+SE7lpmCcLBh/0UYZJpPT4JFg34/INtcFMsrkU36PlvptvvP50=
+PublicExponent: AQAB
+PrivateExponent: C6e08GXphbPPx6j36ZkIZf552gs1XcuVoB4B7hU8P/Qske2QTFOhCwbC8I+qwdtVWNtmuskbpvnVGw9a6X8lh7Z09RIgzO/pI1qau7kyZcuObDOjPw42exmjqISFPIlS1wKA8tw+yVzvZ19vwRk1q6Rne+C1romaUOTkpA6UXsE=
+Prime1: 2mgJ0yr+9vz85abrWBWnB8Gfa1jOw/ccEg8ZToM9GLWI34Qoa0D8Dxm8VJjr1tixXY5zHoWEqRXciTtY3omQDQ==
+Prime2: wmxLpp9rTzU4OREEVwF43b/TxSUBlUq6W83n2XP8YrCm1nS480w4HCUuXfON1ncGYHUuq+v4rF+6UVI3PZT50Q==
+Exponent1: wkdTngUcIiau67YMmSFBoFOq9Lldy9HvpVzK/R0e5vDsnS8ZKTb4QJJ7BaG2ADpno7pISvkoJaRttaEWD3a8rQ==
+Exponent2: YrC8OglEXIGkV3tm2494vf9ozPL6+cBkFsPPg9dXbvVCyyuW0pGHDeplvfUqs4nZp87z8PsoUL+LAUqdldnwcQ==
+Coefficient: mMFr4+rDY5V24HZU3Oa5NEb55iQ56ZNa182GnNhWqX7UqWjcUUGjnkCy40BqeFAQ7lp52xKHvP5Zon56mwuQRw==
+Created: 20140126132645
+Publish: 20140126132645
+Activate: 20140126132645`), "stdin")
+	return s
+}
 
 // keep global port counter that increments with 10 for each
 // new call to newTestServer. The dns server is started on port 'port'
 // the http server is started on 'port+1'.
 var Port = 9490
 var StrPort = "9490" // string equivalent of Port
-
-/* TODO: Tests
-   Test Cluster
-   Test TTL expiration
-   Benchmarks
-*/
-
-func TestAddService(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Host:        "localhost",
-		Environment: "Production",
-		Port:        9000,
-		TTL:         4,
-	}
-
-	b, err := json.Marshal(m)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req, _ := http.NewRequest("PUT", "/skydns/services/123", bytes.NewBuffer(b))
-	resp := httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-
-	if resp.Code != 201 || s.registry.Len() != 1 {
-		t.Fatal("Failed to add service")
-	}
-}
-
-func TestAddServiceDuplicate(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Host:        "localhost",
-		Environment: "Production",
-		Port:        9000,
-		TTL:         4,
-	}
-
-	b, err := json.Marshal(m)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req, _ := http.NewRequest("PUT", "/skydns/services/123", bytes.NewBuffer(b))
-	resp := httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-
-	if resp.Code != http.StatusCreated || s.registry.Len() != 1 {
-		t.Fatal("Failed to add service")
-	}
-
-	req, _ = http.NewRequest("PUT", "/skydns/services/123", bytes.NewBuffer(b))
-	resp = httptest.NewRecorder()
-	s.router.ServeHTTP(resp, req)
-
-	if resp.Code != http.StatusConflict || s.registry.Len() != 1 {
-		t.Fatal("Duplicates should return error code 407", resp.Code)
-	}
-}
-
-func TestRemoveService(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		UUID:        "123",
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Host:        "localhost",
-		Environment: "Production",
-		Port:        9000,
-		TTL:         4,
-	}
-
-	s.registry.Add(m)
-
-	req, _ := http.NewRequest("DELETE", "/skydns/services/"+m.UUID, nil)
-	resp := httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-
-	if resp.Code != http.StatusOK || s.registry.Len() != 0 {
-		t.Fatal("Failed to remove service")
-	}
-}
-
-func TestRemoveUnknownService(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		UUID:        "123",
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Host:        "localhost",
-		Environment: "Production",
-		Port:        9000,
-		TTL:         4,
-	}
-	s.registry.Add(m)
-
-	req, _ := http.NewRequest("DELETE", "/skydns/services/54321", nil)
-	resp := httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-	if resp.Code != http.StatusNotFound || s.registry.Len() != 1 {
-		t.Fatal("API should return 404 when removing unknown service")
-	}
-}
-
-func TestUpdateTTL(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		UUID:        "123",
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Host:        "localhost",
-		Environment: "Production",
-		Port:        9000,
-		TTL:         4,
-	}
-
-	s.registry.Add(m)
-
-	m.TTL = 25
-	b, err := json.Marshal(m)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req, _ := http.NewRequest("PATCH", "/skydns/services/"+m.UUID, bytes.NewBuffer(b))
-	resp := httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-
-	if resp.Code != http.StatusOK {
-		t.Fatal("Failed to update TTL")
-	}
-
-	if serv, err := s.registry.GetUUID(m.UUID); err != nil || serv.TTL != 24 {
-		t.Fatal("Failed to update TTL", err, serv.TTL)
-	}
-}
-
-func TestUpdateTTLUnknownService(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		UUID:        "54321",
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Host:        "localhost",
-		Environment: "Production",
-		Port:        9000,
-		TTL:         4,
-	}
-
-	b, err := json.Marshal(m)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	req, _ := http.NewRequest("PATCH", "/skydns/services/"+m.UUID, bytes.NewBuffer(b))
-	resp := httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-
-	if resp.Code != http.StatusNotFound || s.registry.Len() != 0 {
-		t.Fatal("API should return 404 when updating unknown service")
-	}
-}
 
 func TestGetService(t *testing.T) {
 	s := newTestServer("", "", "")
@@ -400,99 +236,6 @@ func TestHostFailure(t *testing.T) {
 	}
 }
 
-func TestCallback(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Environment: "Production",
-		Host:        "localhost",
-		Port:        9000,
-		TTL:         4,
-	}
-	b, err := json.Marshal(m)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req, _ := http.NewRequest("PUT", "/skydns/services/123", bytes.NewBuffer(b))
-	resp := httptest.NewRecorder()
-	s.router.ServeHTTP(resp, req)
-
-	c := msg.Callback{
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Environment: "Production",
-		Host:        "localhost",
-		Reply:       "localhost",
-		Port:        9650,
-	}
-	b, err = json.Marshal(c)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req, _ = http.NewRequest("PUT", "/skydns/callbacks/101", bytes.NewBuffer(b))
-	resp = httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-	if resp.Code != http.StatusCreated {
-		t.Fatalf("Failed to perform callback: %d", resp.Code)
-	}
-	req, _ = http.NewRequest("DELETE", "/skydns/services/123", nil)
-	resp = httptest.NewRecorder()
-	s.router.ServeHTTP(resp, req)
-	if resp.Code != http.StatusOK {
-		t.Fatal("Failed to remove service")
-		// TODO(miek): check for the callback to be performed
-	}
-}
-
-func TestCallbackFailure(t *testing.T) {
-	s := newTestServer("", "", "")
-	defer s.Stop()
-
-	m := msg.Service{
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Environment: "Production",
-		Host:        "localhost",
-		Port:        9000,
-		TTL:         4,
-	}
-	b, err := json.Marshal(m)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req, _ := http.NewRequest("PUT", "/skydns/services/123", bytes.NewBuffer(b))
-	resp := httptest.NewRecorder()
-	s.router.ServeHTTP(resp, req)
-
-	c := msg.Callback{
-		Name:        "TestService",
-		Version:     "1.0.0",
-		Region:      "Test",
-		Environment: "Testing", // should result in notFound
-		Host:        "localhost",
-		Reply:       "localhost",
-		Port:        9650,
-	}
-	b, err = json.Marshal(c)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req, _ = http.NewRequest("PUT", "/skydns/callbacks/101", bytes.NewBuffer(b))
-	resp = httptest.NewRecorder()
-
-	s.router.ServeHTTP(resp, req)
-	if resp.Code != http.StatusNotFound {
-		t.Fatal("Callback should result in service not found.")
-	}
-}
-
 var services = []msg.Service{
 	{
 		UUID:        "100",
@@ -658,8 +401,6 @@ var dnsTestCases = []dnsTestCase{
 			},
 		},
 	},
-
-	// TODO: Test for case insensitive, this causes an error within the dns package though
 }
 
 type servicesTest struct {
@@ -803,24 +544,6 @@ func TestDNSForward(t *testing.T) {
 	// TODO(miek): DNSSEC DO query
 }
 
-func newTestServer(leader, secret, nameserver string) *Server {
-	members := make([]string, 0)
-
-	p, _ := ioutil.TempDir("", "skydns-test-")
-	if err := os.MkdirAll(p, 0644); err != nil {
-		panic(err.Error())
-	}
-	if leader != "" {
-		members = append(members, leader)
-	}
-
-	Port += 10
-	StrPort = strconv.Itoa(Port)
-	server := NewServer(members, "skydns.local", net.JoinHostPort("127.0.0.1", StrPort), net.JoinHostPort("127.0.0.1", strconv.Itoa(Port+1)), p, 1*time.Second, 1*time.Second, secret, []string{nameserver}, false, "", "")
-	server.Start()
-	return server
-}
-
 // DNSSEC tests
 
 func sectionCheck(t *testing.T, resp []dns.RR, tc []dns.RR) {
@@ -931,70 +654,6 @@ var dnssecTestCases = []dnssecTestCase{
 	},
 }
 
-/*
-	{
-		Question: "region1.*.testservice.production.skydns.local.",
-		Answer: []dns.SRV{
-			{
-				Hdr: dns.RR_Header{
-					Name:   "region1.*.testservice.production.skydns.local.",
-					Ttl:    30,
-					Rrtype: dns.TypeSRV,
-				},
-				Priority: 10,
-				Weight:   100,
-				Target:   "server2.",
-				Port:     9001,
-			},
-			{
-				Hdr: dns.RR_Header{
-					Name:   "region1.*.testservice.production.skydns.local.",
-					Ttl:    33,
-					Rrtype: dns.TypeSRV,
-				},
-				Priority: 20,
-				Weight:   50,
-				Target:   "server5.",
-				Port:     9004,
-			},
-			{
-				Hdr: dns.RR_Header{
-					Name:   "region1.*.testservice.production.skydns.local.",
-					Ttl:    34,
-					Rrtype: dns.TypeSRV,
-				},
-				Priority: 20,
-				Weight:   50,
-				Target:   "server6.",
-				Port:     9005,
-			},
-		},
-	},
-}
-*/
-
-func newTestServerDNSSEC(leader, secret, nameserver string) *Server {
-	s := newTestServer(leader, secret, nameserver)
-	key, _ := dns.NewRR("skydns.local. IN DNSKEY 256 3 5 AwEAAaXfO+DOBMJsQ5H4TfiabwSpqE4cGL0Qlvh5hrQumrjr9eNSdIOjIHJJKCe56qBU5mH+iBlXP29SVf6UiiMjIrAPDVhClLeWFe0PC+XlWseAyRgiLHdQ8r95+AfkhO5aZgnCwYf9FGGSaT0+CRYN+PyDbXBTLK5FN+j5b6bb7z+d")
-	s.dnsKey = key.(*dns.DNSKEY)
-	s.keyTag = s.dnsKey.KeyTag()
-	s.privKey, _ = s.dnsKey.ReadPrivateKey(strings.NewReader(`
-Private-key-format: v1.3
-Algorithm: 5 (RSASHA1)
-Modulus: pd874M4EwmxDkfhN+JpvBKmoThwYvRCW+HmGtC6auOv141J0g6MgckkoJ7nqoFTmYf6IGVc/b1JV/pSKIyMisA8NWEKUt5YV7Q8L5eVax4DJGCIsd1Dyv3n4B+SE7lpmCcLBh/0UYZJpPT4JFg34/INtcFMsrkU36PlvptvvP50=
-PublicExponent: AQAB
-PrivateExponent: C6e08GXphbPPx6j36ZkIZf552gs1XcuVoB4B7hU8P/Qske2QTFOhCwbC8I+qwdtVWNtmuskbpvnVGw9a6X8lh7Z09RIgzO/pI1qau7kyZcuObDOjPw42exmjqISFPIlS1wKA8tw+yVzvZ19vwRk1q6Rne+C1romaUOTkpA6UXsE=
-Prime1: 2mgJ0yr+9vz85abrWBWnB8Gfa1jOw/ccEg8ZToM9GLWI34Qoa0D8Dxm8VJjr1tixXY5zHoWEqRXciTtY3omQDQ==
-Prime2: wmxLpp9rTzU4OREEVwF43b/TxSUBlUq6W83n2XP8YrCm1nS480w4HCUuXfON1ncGYHUuq+v4rF+6UVI3PZT50Q==
-Exponent1: wkdTngUcIiau67YMmSFBoFOq9Lldy9HvpVzK/R0e5vDsnS8ZKTb4QJJ7BaG2ADpno7pISvkoJaRttaEWD3a8rQ==
-Exponent2: YrC8OglEXIGkV3tm2494vf9ozPL6+cBkFsPPg9dXbvVCyyuW0pGHDeplvfUqs4nZp87z8PsoUL+LAUqdldnwcQ==
-Coefficient: mMFr4+rDY5V24HZU3Oa5NEb55iQ56ZNa182GnNhWqX7UqWjcUUGjnkCy40BqeFAQ7lp52xKHvP5Zon56mwuQRw==
-Created: 20140126132645
-Publish: 20140126132645
-Activate: 20140126132645`), "stdin")
-	return s
-}
-
 // newMsg return a new dns.Msg set with DNSSEC and with the question from the tc.
 func newMsg(tc dnssecTestCase) *dns.Msg {
 	m := new(dns.Msg)
@@ -1002,3 +661,4 @@ func newMsg(tc dnssecTestCase) *dns.Msg {
 	m.SetEdns0(4096, true)
 	return m
 }
+*/
