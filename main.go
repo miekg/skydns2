9b3cf12a3e20db6007169e107d0607361b47186f
Miek Gieben <miek@google.com>
2014-07-08 20:13:35 +0100

    Small cleanups after merge:
    rename getEnv to env and add ca-cert flag.

--- main.go
+++ main.go
@@ -16,26 +16,14 @@ import (
 )
 
 var (
-<<<<<<< HEAD
-	machines    = strings.Split(os.Getenv("ETCD_MACHINES"), ",")      // list of URLs to etcd
-	nameservers = strings.Split(os.Getenv("SKYDNS_NAMESERVERS"), ",") // list of nameservers
-	tlskey      = os.Getenv("ETCD_TLSKEY")                            // TLS private key path
-	tlspem      = os.Getenv("ETCD_TLSPEM")                            // X509 certificate
-	cafile      = os.Getenv("ETCD_CAFILE")                            // root CA
-	config      = &Config{ReadTimeout: 0, Domain: "", DnsAddr: "", DNSSEC: ""}
-	nameserver  = ""
-	machine     = ""
-	discover    = false
-	verbose     = false
-=======
 	tlskey     = ""
 	tlspem     = ""
+	cacert     = ""
 	config     = &Config{ReadTimeout: 0, Domain: "", DnsAddr: "", DNSSEC: ""}
 	nameserver = ""
 	machine    = ""
 	discover   = false
 	verbose    = false
->>>>>>> 71a9ea3b8218f8a6316428d42ce7bced829f0f17
 )
 
 const (
@@ -44,7 +32,7 @@ const (
 	RCacheTtl      = 60
 )
 
-func getEnv(key, def string) string {
+func env(key, def string) string {
 	if x := os.Getenv(key); x != "" {
 		return x
 	}
@@ -52,15 +40,15 @@ func getEnv(key, def string) string {
 }
 
 func init() {
-	flag.StringVar(&config.Domain, "domain", getEnv("SKYDNS_DOMAIN", "skydns.local."), "domain to anchor requests to (SKYDNS_DOMAIN)")
-	flag.StringVar(&config.DnsAddr, "addr", getEnv("SKYDNS_ADDR", "127.0.0.1:53"), "ip:port to bind to (SKYDNS_ADDR)")
-	flag.StringVar(&nameserver, "nameservers", getEnv("SKYDNS_NAMESERVERS", ""), "nameserver address(es) to forward (non-local) queries to e.g. 8.8.8.8:53,8.8.4.4:53")
-	flag.StringVar(&machine, "machines", getEnv("ETCD_MACHINES", ""), "machine address(es) running etcd")
+	flag.StringVar(&config.Domain, "domain", env("SKYDNS_DOMAIN", "skydns.local."), "domain to anchor requests to (SKYDNS_DOMAIN)")
+	flag.StringVar(&config.DnsAddr, "addr", env("SKYDNS_ADDR", "127.0.0.1:53"), "ip:port to bind to (SKYDNS_ADDR)")
+	flag.StringVar(&nameserver, "nameservers", env("SKYDNS_NAMESERVERS", ""), "nameserver address(es) to forward (non-local) queries to e.g. 8.8.8.8:53,8.8.4.4:53")
+	flag.StringVar(&machine, "machines", env("ETCD_MACHINES", ""), "machine address(es) running etcd")
 	flag.StringVar(&config.DNSSEC, "dnssec", "", "basename of DNSSEC key file e.q. Kskydns.local.+005+38250")
 	flag.StringVar(&config.Local, "local", "", "optional unique value for this skydns instance")
-	flag.StringVar(&tlskey, "tls-key", getEnv("ETCD_TLSKEY", ""), "TLS Private Key path")
-	flag.StringVar(&tlspem, "tls-pem", getEnv("ETCD_TLSPEM", ""), "X509 Certificate")
-	flag.StringVar(&cafile, "ca-file", getEnv("ECTD_CACERT", "") , "CA Certificate")
+	flag.StringVar(&tlskey, "tls-key", env("ETCD_TLSKEY", ""), "TLS Private Key path")
+	flag.StringVar(&tlspem, "tls-pem", env("ETCD_TLSPEM", ""), "X509 Certificate")
+	flag.StringVar(&cacert, "ca-cert", env("ECTD_CACERT", "") , "CA Certificate")
 	flag.DurationVar(&config.ReadTimeout, "rtimeout", 2*time.Second, "read timeout")
 	flag.BoolVar(&config.RoundRobin, "round-robin", true, "round robin A/AAAA replies")
 	flag.BoolVar(&discover, "discover", false, "discover new machines by watching /v2/_etcd/machines")
