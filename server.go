3acc5bc335024e67e41c22944ead2a33050d0dc5
Miek Gieben <miek@google.com>
2014-07-15 08:38:05 +0100

    Put all forwarding functions in forwarding.go

--- server.go
+++ server.go
@@ -303,72 +303,6 @@ func (s *server) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
 	}
 }
 
-// ServeDNSForward forwards a request to a nameservers and returns the response.
-func (s *server) ServeDNSForward(w dns.ResponseWriter, req *dns.Msg) {
-	StatsForwardCount.Inc(1)
-	if len(s.config.Nameservers) == 0 || dns.CountLabel(req.Question[0].Name) <= s.config.Ndots {
-		s.config.log.Infof("no nameservers defined or name too short, can not forward")
-		m := new(dns.Msg)
-		m.SetReply(req)
-		m.SetRcode(req, dns.RcodeServerFailure)
-		m.Authoritative = false     // no matter what set to false
-		m.RecursionAvailable = true // and this is still true
-		w.WriteMsg(m)
-		return
-	}
-	network := "udp"
-	if _, ok := w.RemoteAddr().(*net.TCPAddr); ok {
-		network = "tcp"
-	}
-
-	// Try again, when it fails?
-	c := &dns.Client{Net: network, ReadTimeout: 2 * s.config.ReadTimeout, WriteTimeout: 2 * s.config.ReadTimeout}
-
-	// Use request Id for "random" nameserver selection.
-	nsid := int(req.Id) % len(s.config.Nameservers)
-	try := 0
-Redo:
-	r, _, err := c.Exchange(req, s.config.Nameservers[nsid])
-	if err == nil {
-		r.Compress = true
-		w.WriteMsg(r)
-		return
-	}
-	// Seen an error, this can only mean, "server not reached", try again
-	// but only if we have not exausted our nameservers.
-	if try < len(s.config.Nameservers) {
-		try++
-		nsid = (nsid + 1) % len(s.config.Nameservers)
-		goto Redo
-	}
-
-	s.config.log.Errorf("failure to forward request %q", err)
-	m := new(dns.Msg)
-	m.SetReply(req)
-	m.SetRcode(req, dns.RcodeServerFailure)
-	w.WriteMsg(m)
-}
-
-// ServeDNSReverse is the handler for DNS requests for the reverse zone. If nothing is found
-// locally the request is forwarded to the forwarder for resolution.
-func (s *server) ServeDNSReverse(w dns.ResponseWriter, req *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(req)
-	m.Compress = true
-	m.Authoritative = false // Set to false, because I don't know what to do wrt DNSSEC.
-	m.RecursionAvailable = true
-	var err error
-	if m.Answer, err = s.PTRRecords(req.Question[0]); err == nil {
-		// TODO(miek): Reverse DNSSEC. We should sign this, but requires a key....and more
-		// Probably not worth the hassle?
-		if err := w.WriteMsg(m); err != nil {
-			s.config.log.Errorf("failure to return reply %q", err)
-		}
-	}
-	// Always forward if not found locally.
-	s.ServeDNSForward(w, req)
-}
-
 func (s *server) AddressRecords(q dns.Question, name string, previousRecords []dns.RR) (records []dns.RR, err error) {
 	path, star := msg.PathWithWildcard(name)
 	r, err := s.client.Get(path, false, true)
@@ -779,51 +713,6 @@ func (s *server) calculateTtl(node *etcd.Node, serv *msg.Service) uint32 {
 		return etcdTtl
 	}
 	return serv.Ttl
-}
-
-// Lookup looks up name,type using the recursive nameserver defines
-// in the server's config. If none defined it returns an error
-func (s *server) Lookup(n string, t, dnssec uint16) (*dns.Msg, error) {
-	StatsLookupCount.Inc(1)
-	if len(s.config.Nameservers) == 0 {
-		return nil, fmt.Errorf("no nameservers configured can not lookup name")
-	}
-	if dns.CountLabel(n) <= s.config.Ndots {
-		return nil, fmt.Errorf("name has fewer than three labels")
-	}
-	m := new(dns.Msg)
-	m.SetQuestion(n, t)
-	if dnssec > 0 {
-		m.SetEdns0(dnssec, true)
-	}
-
-	c := &dns.Client{Net: "udp", ReadTimeout: 2 * s.config.ReadTimeout, WriteTimeout: 2 * s.config.ReadTimeout}
-	nsid := int(m.Id) % len(s.config.Nameservers)
-	try := 0
-Redo:
-	r, _, err := c.Exchange(m, s.config.Nameservers[nsid])
-	if err == nil {
-		if r.Rcode != dns.RcodeSuccess {
-			return nil, fmt.Errorf("rcode is not equal to success")
-		}
-		// Reset TTLs to rcache TTL to make some of the other code
-		// and the tests not care about TTLs
-		for _, rr := range r.Answer {
-			rr.Header().Ttl = uint32(s.config.RCacheTtl)
-		}
-		for _, rr := range r.Extra {
-			rr.Header().Ttl = uint32(s.config.RCacheTtl)
-		}
-		return r, nil
-	}
-	// Seen an error, this can only mean, "server not reached", try again
-	// but only if we have not exausted our nameservers.
-	if try < len(s.config.Nameservers) {
-		try++
-		nsid = (nsid + 1) % len(s.config.Nameservers)
-		goto Redo
-	}
-	return nil, fmt.Errorf("failure to lookup name")
 }
 
 func (s *server) NameError(m, req *dns.Msg) {
